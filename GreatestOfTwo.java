6f937585c2c86fb15f90c0d866e301e07823f296
Gajula Yugandhar <65944758+YugandharGajula@users.noreply.github.com>
2020-05-26 23:03:47 +0530

    Add files via upload

---
+++ GreatestOfTwo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class GreatestOfTwo
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int input1=s.nextInt();
+int input2=s.nextInt();
+if(input1>input2)
+{
+System.out.println(input1);
+}else
+{
+System.out.println(input2);
+}
+}
+}
