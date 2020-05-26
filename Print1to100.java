6e6b98433516e6d79d52220cda6c254f618d80ad
Gajula Yugandhar <65944758+YugandharGajula@users.noreply.github.com>
2020-05-26 21:27:19 +0530

    Add files via upload

---
+++ Print1to100.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Print1to100
+{
+    public static void main(String[]args)
+    {
+
+        for(int i=1;i<=100;i++)
+        {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+}
