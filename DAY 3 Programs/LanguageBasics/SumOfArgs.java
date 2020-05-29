4b4bda3eb55d148dc217f63472437e4c3781d5e3
Gajula Yugandhar <65944758+YugandharGajula@users.noreply.github.com>
2020-05-29 13:27:10 +0530

    Create SumOfArgs.java

---
+++ DAY 3 Programs/LanguageBasics/SumOfArgs.java
@@ -0,0 +1,10 @@
+public class SumOfArgs
+{
+public static void main(String args[])
+{
+int input1=Integer.parseInt(args[0]);
+int input2=Integer.parseInt(args[1]);
+int sum=input1+input2;
+System.out.println("The Sum of "+args[0]+" and " +args[1]+" is " +" "+sum);
+}
+}
